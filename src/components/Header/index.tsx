448270db8bf106f78c7927a63a0f4c9ea8ca9799
Arthur <peraarthur2018@gmail.com>
2022-09-27 17:26:54 -0300

    new addon

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import { Coins } from "phosphor-react";
 import { HeaderContainer, HeaderContent, LogoContent, NewTransactionButton, TextLogo } from "./styles";
+import * as Dialog from '@radix-ui/react-dialog';
 
 export function Header() {
     return(
@@ -10,7 +11,20 @@ export function Header() {
                 <TextLogo>Financial Controller </TextLogo>
             </LogoContent>
 
-            <NewTransactionButton>Nova transição</NewTransactionButton>
+           <Dialog.Root>
+            <Dialog.Trigger asChild>
+                <NewTransactionButton>Nova transição</NewTransactionButton>
+            </Dialog.Trigger>
+            <Dialog.Portal>
+                <Dialog.Overlay />
+
+                <Dialog.Content>
+                    <Dialog.Title>Nova Transação</Dialog.Title>
+
+                    <Dialog.Close />
+                </Dialog.Content>
+            </Dialog.Portal>
+           </Dialog.Root>
         </HeaderContent>
     </HeaderContainer>
     )
