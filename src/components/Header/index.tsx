b398fbd342428bf9c3cc6775bccd5eb98eb51588
Arthur <peraarthur2018@gmail.com>
2022-09-28 14:04:03 -0300

    style modal + atualization style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import { Coins } from "phosphor-react";
 import { HeaderContainer, HeaderContent, LogoContent, NewTransactionButton, TextLogo } from "./styles";
 import * as Dialog from '@radix-ui/react-dialog';
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
     return(
@@ -15,15 +16,7 @@ export function Header() {
             <Dialog.Trigger asChild>
                 <NewTransactionButton>Nova transição</NewTransactionButton>
             </Dialog.Trigger>
-            <Dialog.Portal>
-                <Dialog.Overlay />
-
-                <Dialog.Content>
-                    <Dialog.Title>Nova Transação</Dialog.Title>
-
-                    <Dialog.Close />
-                </Dialog.Content>
-            </Dialog.Portal>
+            <NewTransactionModal />
            </Dialog.Root>
         </HeaderContent>
     </HeaderContainer>
