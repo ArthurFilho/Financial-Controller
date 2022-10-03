1900a504be52c1b010a652b92daff39de687be71
Arthur <peraarthur2018@gmail.com>
2022-10-03 14:58:03 -0300

    api test

--- src/Pages/Transactions/index.tsx
+++ src/Pages/Transactions/index.tsx
@@ -2,8 +2,32 @@ import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
+import { useState , useEffect } from "react"
+
+interface Transaction {
+    id: number;
+    description: string;
+    type: 'income' | 'outcome';
+    price: number;
+    category: string;
+    createdAt: string;
+}
 
 export function Transactions() {
+    const [ transactions , setTransactions ] = useState<Transaction[]>([])
+
+    async function loadTransactions ( ) {
+    const response = await fetch ('http://localhost:3000/transactions')
+    const data = await response.json();
+    
+    setTransactions(data);
+    }
+    
+    useEffect ( () => {
+        loadTransactions();
+    }, [])
+                          
+
     return(
     <div>
      <Header />
@@ -13,26 +37,21 @@ export function Transactions() {
         <SearchForm />
         <TransactionsTable>
             <tbody>
+                {transactions.map(transaction => {
+                    return (
                 <tr>
-                    <td width="50%">Desenvolvimento de site</td>
-                    <td>
-                        <PriceHighLight variant="income">
-                        R$ 12,000,00
-                        </PriceHighLight>    
-                    </td>
-                    <td>Venda</td>
-                    <td>13/04/2022</td>
+                <td width="50%">{transaction.description}</td>
+                <td>
+                <PriceHighLight variant={transaction.type}>
+                {transaction.price}
+                </PriceHighLight>    
+                </td>
+                <td>{transaction.category}</td>
+                <td>{transaction.createdAt}</td>
                 </tr>
-                <tr>
-                    <td width="50%">Hamburguer</td>
-                    <td>
-                    <PriceHighLight variant="outcome">
-                        - R$ 59,00
-                    </PriceHighLight>        
-                    </td>
-                    <td>Alimentação</td>
-                    <td>10/04/2022</td>
-                </tr>
+                )
+                })}
+                
             </tbody>
         </TransactionsTable>
         
